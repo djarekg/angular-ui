0be9dafcb71344f2c46098adab0f6098588dfd9d
Dustin Griffith <djarekg@gmail.com>
2025-08-23 23:36:35 -0400

    feature: added user list filtering by active

--- apps/demo/src/app/components/user-list/user-list.component.ts
+++ apps/demo/src/app/components/user-list/user-list.component.ts
@@ -1,11 +1,20 @@
 import { UserService } from '@/core/auth/user.service.js';
-import { ChangeDetectionStrategy, Component, inject, resource } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  resource,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
-  imports: [MatCardModule],
+  imports: [FormsModule, MatCardModule, MatButtonToggleModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,8 +22,28 @@ import { Router } from '@angular/router';
 export class UserListComponent {
   readonly #router = inject(Router);
   readonly #userService = inject(UserService);
-  protected readonly usersResource = resource({
+  readonly #usersResource = resource({
     loader: this.#userService.getUsers,
+  });
+
+  protected readonly usersActiveFilter = signal<'all' | 'true' | 'false'>('all');
+  protected readonly users = computed(() => {
+    // Filter users' isActive by selected filter value.
+    if (this.#usersResource.hasValue()) {
+      const filter = this.usersActiveFilter();
+      const users = this.#usersResource.value().filter(({ isActive }) => {
+        if (filter === 'all') {
+          return true;
+        }
+        else {
+          return isActive === (filter === 'true');
+        }
+      });
+
+      return users;
+    }
+
+    return [];
   });
 
   protected onCardClick(id: string) {
